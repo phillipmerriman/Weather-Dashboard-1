6a363d8bfb632d77417c81d8a28ff5a152b9007e
Phillip Merriman <phillipmerriman@gmail.com>
2020-11-04 12:44:05 -0600

    Got all requirements finished.

--- script.js
+++ script.js
@@ -52,8 +52,6 @@ let apiKey = "5602f605cfcea993a0617227f0c3e839";
 let date = dayjs().format("MMM DD, YYYY");
 let searchNumber = localStorage.length;
 
-// function getWeather (city) {}
-
 //Get recent searches from localStorage and render as an anchor tag to #recent div
 for (let i = 0; i <= localStorage.length; i++) {
   let newAnchorEl = $(`<a href="#" class="row border bg-light previous-search">`)
@@ -209,15 +207,151 @@ $("button").on("click", function (e) {
           $(`#${i}-hum`).text(`Humidity: ${fHumidity}`);
         }
       }
-
-      // console.log(j);
-      // console.log("icon");
-      // console.log(Math.floor(farenheit) + " ° F");
-      // console.log(response.list[j].main.humidity);
     }
   });
 });
 
 $(document).on("click", ".previous-search", function (e) {
   e.preventDefault();
+
+  console.log($(this).text());
+
+  let currentCity = $(this).text();
+  let currentUrl = `http://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}`;
+
+  $.ajax({
+    url: currentUrl,
+    method: "GET",
+  }).fail(function () {
+    alert('Please enter a valid city.');
+    return;
+  }).then(function (response) {
+
+    let lat = response.coord.lat;
+    let lon = response.coord.lon;
+    let oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    let iconHtml = `https://openweathermap.org/img/wn/`;
+    let imageName = response.weather[0].icon;
+    let imgUrl = iconHtml + imageName + ".png";
+
+    $.ajax({
+      url: oneCallUrl,
+      method: "GET",
+    }).then(function (response) {
+      let temp = (response.current.temp - 273.15) * 1.8 + 32;
+      let humidity = response.current.humidity;
+      let wind = response.current.wind_speed;
+      let uvindex = response.current.uvi;
+
+      //thanks to my classmates Spencer Rosio and Caleb Walker for the help with this weather image!
+      $("#icon-today").attr("src", imgUrl);
+
+      $("#current-day").text(`${currentCity} (${date})`);
+      $("#current-temp").text(`Temperature: ${Math.floor(temp)}° F`);
+      $("#current-humidity").text(`Humidity: ${humidity}`);
+      $("#current-wind").text(`Wind Speed: ${wind} mph`);
+      $("#current-uv").empty();
+      $("#current-uv").text("UV-Index: ");
+      $("#current-uv").append(
+        $(`<p class='border' id="uv-condition">${uvindex}<p>`)
+      );
+
+      //set uvindex color based on uv-condition
+      if (uvindex < 3) {
+        $("#uv-condition").css("background-color", "skyblue");
+      } else if (uvindex < 6) {
+        $("#uv-condition").css("background-color", "yellow");
+      } else if (uvindex < 8) {
+        $("#uv-condition").css("background-color", "orange");
+      } else if (uvindex < 11) {
+        $("#uv-condition").css("background-color", "red");
+      } else {
+        $("#uv-condition").css("background-color", "rebeccapurple");
+      }
+
+      //empty search field
+      $("#city-search").val("");
+    });
+  });
+
+  
+  
+
+  //get the 5 day forecast
+  let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=${apiKey}`;
+
+  $.ajax({
+    url: forecastUrl,
+    method: "GET",
+  }).then(function (response) {
+    
+    for (let i = 1; i < 6; i++) {
+
+      let year = Date().substr(11, 4);
+      let month = Date().substr(4, 3);
+      let day = parseInt(Date().substr(8, 2)) + i;
+
+      switch (month) {
+        case "Jan":
+          month = 01;
+          break;
+        case "Feb":
+          month = 02;
+          break;
+        case "Mar":
+          month = 03;
+          break;
+        case "Apr":
+          month = 04;
+          break;
+        case "May":
+          month = 05;
+          break;
+        case "Jun":
+          month = 06;
+          break;
+        case "Jul":
+          month = 07;
+          break;
+        case "Aug":
+          month = 08;
+          break;
+        case "Sep":
+          month = 09;
+          break;
+        case "Oct":
+          month = 10;
+          break;
+        case "Nov":
+          month = 11;
+          break;
+        default:
+          month = 12;
+      }
+
+      let exactDate = `${year}-${month}-${day < 10 ? "0" + day : day}`;
+
+      // this console.log breaks stuff
+      // console.log(response.list[j].dt_txt.substr(0, 10));
+
+      for (let k = 0; k < response.list.length; k++) {
+        if (response.list[k].dt_txt.substr(0, 10) === exactDate) {
+
+          let futureDay = response.list[k].dt_txt.substr(0, 10);
+          let fIconHtml = `https://openweathermap.org/img/wn/`;
+          let fIcon = response.list[k].weather[0].icon;
+          let fImgUrl = fIconHtml + fIcon + ".png";
+          let farenheit = (response.list[k].main.temp - 273.15) * 1.8 + 32;
+          let fHumidity = response.list[k].main.humidity;
+
+          // $(`#${i}`).empty();
+          $(`#${i}-date`).text(futureDay);
+          $(`#${i}-icon`).attr("src", fImgUrl); 
+          $(`#${i}-icon`).attr("alt", "weather icon")
+          $(`#${i}-temp`).text(`Temp: ${Math.floor(farenheit)}° F`);
+          $(`#${i}-hum`).text(`Humidity: ${fHumidity}`);
+        }
+      }
+    }
+  });
 });
